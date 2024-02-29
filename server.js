d907fe05f96af1cdbb726add174f1bc0c3d7eb42
Omkar Ware <123940891+ifeelomii@users.noreply.github.com>
2024-02-29 23:40:32 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mysql = require("mysql");
 const cors = require("cors");
 const app = express();
 
+app.use(express.json());
 app.use(cors())
 
 const db = mysql.createConnection({
@@ -12,18 +13,31 @@ const db = mysql.createConnection({
   database: "whattodo",
   insecureAuth: true
 });
-app.get("/", (req, res) => { return res.json("From Backend") } );
+app.get("/", (req, res) => { return res.json("Connected to Backend") } );
 app.get("/activities", (req, res) => { 
     const sql = "SELECT * FROM activities";
     db.query(sql, (err,data) => {
-        // console.log(data)
-        // console.log(err)
         if (err) return res.json(err);
         return res.json(data);
     })
+});
 
-    // return res.json("inside")
- } );
+app.post("/activities", (req, res) => {
+    const {activity}  = req.body; 
+    // return res.json(activity)
+    // console.log(activity)
+  const sql = `INSERT INTO activities (activity) VALUES (?)`;
+  db.query(sql, [activity], (err, result) => {
+    if (err) {
+      res
+        .status(500)
+        .json({ error: "Failed to insert activity into database" });
+      throw err;
+    }
+    console.log("Activity inserted successfully");
+    res.status(200).json({ message: "Activity inserted successfully" });
+  });
+});
 
 app.listen(8080, () => {
     console.log("Server is listening on port 8080");
